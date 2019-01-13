2202d405faa112c24d3db9cbbc8ebccbcafbd967
Edward Feng <edward0414@gmail.com>
2019-01-12 21:08:00 -0500

    update token secret

--- server/server-api/libraries/utilities.js
+++ server/server-api/libraries/utilities.js
@@ -3,7 +3,13 @@ const slack = require("slack");
 const roomOpener = ({ userId }) => {
   return new Promise((resolve, reject) => {
     slack.im
-      .open({ token: process.env.BTOKEN, user: userId })
+      .open({
+        token:
+          process.env.ENV === "development"
+            ? process.env.DEV_BTOKEN
+            : process.env.PROD_BTOKEN,
+        user: userId
+      })
       .then(resolve)
       .catch(reject);
   });
@@ -12,7 +18,13 @@ const roomOpener = ({ userId }) => {
 const userQuery = ({ userId }) => {
   return new Promise((resolve, reject) => {
     slack.users
-      .info({ token: process.env.BTOKEN, user: userId })
+      .info({
+        token:
+          process.env.ENV === "development"
+            ? process.env.DEV_BTOKEN
+            : process.env.PROD_BTOKEN,
+        user: userId
+      })
       .then(resolve)
       .catch(reject);
   });
